cffc2b91a6d532c8695394bebb1d00d738bed54a
Алексей <alexust11@yandex.ru>
2022-10-26 12:57:55 +0700

    D/Z 2-1

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
+import java.security.KeyStore;
 import java.util.*;
-
-import static java.lang.Math.random;
-
+import java.util.Arrays;
+import java.util.List;
 public class Main {
     private static Map<String, List<Integer>> task=new HashMap<>();
+    private static Map<String, Integer> task1=new HashMap<>();
+
     private static Random random=new Random();
+    private static String summa;
+
     public static void main(String[] args) {
-
-
-        task.put("string1", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
-        task.put("string2", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
-        task.put("string3", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
-        task.put("string4", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
-        task.put("string5", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
-
-        System.out.println(task);
-
 //        Создайте Map<String, List<Integer>>. Заполните ее 5 элементами, где ключ — произвольное значение,
 //        а значение — список, состоящий из 3 случайных чисел в диапазоне от 0 до 1000.
 //        Преобразуйте созданную коллекцию в новую — Map<String, Integer>, где ключи взяты из первой коллекции,
@@ -27,5 +21,49 @@ public class Main {
 //        "string1" —> 19
 //        "string2" —> 700
 
+        task.put("string1", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
+        task.put("string2", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
+        task.put("string3", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
+        task.put("string4", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
+        task.put("string5", Arrays.asList(random.nextInt(1000), random.nextInt(1000), random.nextInt(1000)));
+
+        printOldtMap(task);
+        transformate(task);
+        printNewtMap(task1);
+
+
+
     }
-}
+
+    private static void transformate(Map <String,List<Integer>> map) {
+        List<Integer> list;
+
+        for(Map.Entry<String,List<Integer>> entry: map.entrySet()) {
+            int s=0;
+            String key = entry.getKey();
+            // get value
+            list = entry.getValue();
+            for (int i = 0; i < list.size(); i++) {
+                s=s+list.get(i);
+
+            }
+
+            task1.put(key+"—>", s);
+        }
+
+    }
+
+    private static void printNewtMap(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> pair: map.entrySet()) {
+            System.out.println(String.format(pair.getKey()+pair.getValue()));
+        }
+
+    }
+    private static void printOldtMap(Map<String, List<Integer>> map) {
+        for (Map.Entry<String, List<Integer>> pair: map.entrySet()) {
+            System.out.println(String.format(pair.getKey()+"->"+pair.getValue()));
+        }
+
+    }
+    }
+
